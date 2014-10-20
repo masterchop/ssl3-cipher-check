66cf75ee5b3ed5574f34d3adf7b307879a0cad60
lamar.spells@gmail.com <lamar.spells@gmail.com@64e1afc0-e438-3407-328e-fa53b047c2f6>
2014-10-20 13:48:10 +0000

    Reliability improvement.  Changed nmap to use connect and added error code checking to detect connection errors.

--- ssl3_scan.sh
+++ ssl3_scan.sh
@@ -44,18 +44,21 @@ if [ $# -eq 1 ] ; then
 fi
 
 echo "Beginning test... please be patient..."
-nmap -PN -sS -p 443,4443,14443,8443,18443 -v  $IP_RANGE | grep ^Discovered | awk '{printf("%s:%s\n",$NF,$4)}'  | sed -e 's!/tcp!!g' | while read hn
+nmap -PN -sT -p 443,4443,14443,8443,18443 -v  $IP_RANGE | grep ^Discovered | awk '{printf("%s:%s\n",$NF,$4)}'  | sed -e 's!/tcp!!g' | while read hn
 do
    retcd=0
    $SSL3_CHECK_CIPHERS $hn > /dev/null 2>&1
    retcd=$?
 
    if [ $retcd -eq 0 ] ; then
-      echo "$hn - SSL3.0 ciphers NOT supported"
-   else 
-      echo "$hn - SSL3.0 ciphers supported"
+      echo "$hn - SSL3.0 NOT supported"
+   else
+      if [ $retcd -eq 1 ] ; then
+         echo "$hn - SSL3.0 supported"
+      else
+         echo "$hn - Error in connection"
+      fi
    fi
-
 done
 exit 0
 
